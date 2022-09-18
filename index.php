4bb09279c0f9de0595607841769694f34b845d12
miwanoff <marina.iva.noffa@gmail.com>
2022-09-18 14:55:47 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$day = 12;
+switch ($day) {
+    case 1:echo "Понеділок";
+        break;
+    case 2:echo "Вівторок";
+        break;
+    case 3:echo "Середа";
+        break;
+    default:echo "<span style=\"color:red;\">Помилка</span>";
+}
